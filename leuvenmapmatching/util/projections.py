7e353c29a96d9cdf4ef2c94f98a907881f73156d
Wannes Meert <wannes.meert@cs.kuleuven.be>
2018-07-10 11:24:09 +0200

    projections

--- leuvenmapmatching/util/projections.py
+++ leuvenmapmatching/util/projections.py
@@ -13,35 +13,47 @@ import logging
 import pyproj
 
 logger = logging.getLogger("be.kuleuven.cs.dtai.mapmatching")
-phi_er = 0
-lambda_er = 0
 
 
-def latlon2equirectangular(lat, lon):
+def latlon2equirectangular(lat, lon, phi_er=0, lambda_er=0):
     """Naive equirectangular projection.  This is the same as considering (lat,lon) == (y,x).
     This is a lot faster but only works if you are far enough from the poles and the dateline.
+
+    :param lat:
+    :param lon:
+    :param phi_er: The standard parallels (north and south of the equator) where the scale of the projection is true
+    :param lambda_er: The central meridian of the map
     """
     x = (lon - lambda_er) * math.cos(phi_er)
     y = lat - phi_er
     return y, x
 
 
-def equirectangular2latlon(y, x):
+def equirectangular2latlon(y, x, phi_er=0, lambda_er=0):
     """Naive equirectangular projection. This is the same as considering (lat,lon) == (y,x).
     This is a lot faster but only works if you are far enough from the poles and the dateline.
+
+    :param phi_er: The standard parallels (north and south of the equator) where the scale of the projection is true
+    :param lambda_er: The central meridian of the map
     """
     lon = x / math.cos(phi_er) + lambda_er
     lat = y + phi_er
     return lat, lon
 
 
-def latlon2grs80(coordinates):
-    """Given a list of (lon, lat) coordinates, create x-y coordinates in meter."""
-    lon_0, lat_0 = coordinates[0]
-    proj = pyproj.Proj(f"+proj=merc +ellps=GRS80 +units=m +lon_0={lon_0} +lat_0={lat_0} +lat_ts={lat_0} +no_defs")
-    xs, ys = [], []
+def latlon2grs80(coordinates, lon_0=0.0, lat_ts=0.0, y_0=0, x_0=0.0, **kwargs):
+    """Given a list of (lon, lat) coordinates, create x-y coordinates in meter.
+
+    :param coordinates: A list of lon-lat tuples
+    :param lon_0: Longitude of projection center.
+    :param lat_ts: Latitude of true scale. Defines the latitude where scale is not distorted.
+    :param y_0: False northing
+    :param x_0: False easting
+    """
+    other_options = " ".join(f"+{key}={val}" for key, val in kwargs.items())
+    proj = pyproj.Proj(f"+proj=utm +ellps=GRS80 +units=m "
+                       f"+lon_0={lon_0} +lat_ts={lat_ts} +y_0={y_0} +x_0={x_0} "
+                       f"+no_defs {other_options}")
     for lon, lat in coordinates:
         x, y = proj(lon, lat)
-        xs.append(x)
-        ys.append(y)
-    return zip(xs, ys)
+        yield x, y
--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -3,10 +3,11 @@
 import sys
 import logging
 from datetime import datetime
-
+import pytest
 
 import leuvenmapmatching as mm
 from leuvenmapmatching.util.gpx import path_to_gpx
+from leuvenmapmatching.util.projections import latlon2grs80
 
 
 def test_path_to_gpx():
@@ -14,13 +15,25 @@ def test_path_to_gpx():
     gpx = path_to_gpx(path)
 
     assert len(path) == len(gpx.tracks[0].segments[0].points)
-    assert path[0][0] == gpx.tracks[0].segments[0].points[0].latitude
-    assert path[0][1] == gpx.tracks[0].segments[0].points[0].longitude
-    assert path[0][2] == gpx.tracks[0].segments[0].points[0].time
+    assert path[0][0] == pytest.approx(gpx.tracks[0].segments[0].points[0].latitude)
+    assert path[0][1] == pytest.approx(gpx.tracks[0].segments[0].points[0].longitude)
+    assert path[0][2] == pytest.approx(gpx.tracks[0].segments[0].points[0].time)
+
+
+def test_grs80():
+    coordinates = [(4.67878, 50.864), (4.68054, 50.86381), (4.68098, 50.86332), (4.68129, 50.86303), (4.6817, 50.86284),
+                   (4.68277, 50.86371), (4.68894, 50.86895), (4.69344, 50.86987), (4.69354, 50.86992),
+                   (4.69427, 50.87157), (4.69643, 50.87315), (4.69768, 50.87552), (4.6997, 50.87828)]
+    points = latlon2grs80(coordinates, lon_0=coordinates[0][0], lat_ts=coordinates[0][1])
+    points = list(points)
+    point = points[0]
+    assert point[0] == pytest.approx(618139.9385518166)
+    assert point[1] == pytest.approx(5636043.991970774)
 
 
 if __name__ == "__main__":
-    mm.matching.logger.setLevel(logging.INFO)
-    # mm.matching.logger.setLevel(logging.DEBUG)
+    # mm.matching.logger.setLevel(logging.INFO)
+    mm.matching.logger.setLevel(logging.DEBUG)
     mm.matching.logger.addHandler(logging.StreamHandler(sys.stdout))
-    test_path_to_gpx()
+    # test_path_to_gpx()
+    test_grs80()
