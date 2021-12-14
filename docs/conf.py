209e24c9686d763e3ba4fe1538ec8f8426ce64c0
Wannes Meert <wannes.meert@cs.kuleuven.be>
2021-12-14 18:48:42 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- General configuration ------------------------------------------------
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Leuven.MapMatching'
-copyright = '2018, Wannes Meert'
+copyright = '2018-2021, Wannes Meert'
 author = 'Wannes Meert'
 
 # The version info for the project you're documenting, acts as replacement for
